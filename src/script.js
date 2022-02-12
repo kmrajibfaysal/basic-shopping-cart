5f49d1e77aefb03a8041203ef15129e93fe7cf9b
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-13 02:15:46 +0600

    added keyup event listener in the input field of quantity

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-// common function to update value of count
+// common function to update value of count using button
 function updateProductNumber(product, isIncreasing, price) {
     const productInput = document.querySelector(`#${product}-count`);
     let productNumber = productInput.value;
@@ -15,7 +15,8 @@ function updateProductNumber(product, isIncreasing, price) {
     productTotal.innerText = productNumber * price;
 }
 
-// increase decrease event handler
+
+// phone increase decrease event handler
 document.querySelector('#phone-plus').addEventListener('click', () => {
     updateProductNumber('phone', true, 1219);
 });
@@ -24,10 +25,26 @@ document.querySelector('#phone-minus').addEventListener('click', () => {
     updateProductNumber('phone', false, 1219);
 });
 
+document.querySelector('#phone-count').addEventListener('keyup', () => {
+    const inputQ = document.querySelector('#phone-count');
+    const count = parseInt(inputQ.value);
+    if (count > 0) {
+        document.querySelector('#phone-total').innerText = count * 1219;
+    }
+});
+
 // case increase decrease event handler
 document.querySelector('#case-plus').addEventListener('click', () => {
     updateProductNumber('case', true, 59);
 });
 document.querySelector('#case-minus').addEventListener('click', () => {
     updateProductNumber('case', false, 59);
+});
+
+document.querySelector('#case-count').addEventListener('keyup', () => {
+    const inputQ = document.querySelector('#case-count');
+    const count = parseInt(inputQ.value);
+    if (count > 0) {
+        document.querySelector('#case-total').innerText = count * 59;
+    }
 });
