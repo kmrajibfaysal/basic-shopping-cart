ca96c44bf69ac4d2a39ef6863041eea13ad500d8
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-13 01:58:11 +0600

    added increment and decrement button event handler function and removed extra code to make it DRY

--- src/script.js
+++ src/script.js
@@ -1,51 +1,33 @@
-// window.onload = () => {
-//     main();
-// };
-
-// function main() {
-//     // alert('hi');
-// }
-
-// const casePlus = document.querySelector('#case-plus');
-// const caseMinus = document.querySelector('#case-minus');
-// const phoneQuantity = document.querySelector('#case-count-phone');
-// const phonePrice = document.querySelector('#phone-price');
-
-// casePlus.addEventListener('click', () => {
-//     let phoneCount = parseInt(phoneQuantity.value);
-//     phoneCount += 1;
-//     phoneQuantity.value = phoneCount;
-//     const phoneTotalPrice = parseInt(phonePrice.innerText);
-//     phonePrice.innerText = phoneTotalPrice * phoneQuantity.value;
-// });
-
-// caseMinus.addEventListener('click', () => {
-//     if (phoneQuantity.value > 0) {
-//         let phoneCount = parseInt(phoneQuantity.value);
-//         phoneCount -= 1;
-//         phoneQuantity.value = phoneCount;
-//     }
-// });
-
 // common function to update value of count
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.querySelector('#case-count-phone');
-    const caseNumber = caseInput.value;
+function updateProductNumber(product, isIncreasing, price) {
+    const productInput = document.querySelector(`#${product}-count`);
+    let productNumber = productInput.value;
+    console.log(productNumber);
     if (isIncreasing) {
-        caseInput.value = parseInt(caseNumber) + 1;
+        productNumber = parseInt(productNumber) + 1;
     }
-    if (!isIncreasing) {
-        caseInput.value = parseInt(caseNumber) - 1;
+    if (!isIncreasing && productNumber > 1) {
+        productNumber = parseInt(productNumber) - 1;
     }
+    productInput.value = productNumber;
+    // update product total
+    const productTotal = document.querySelector(`#${product}-total`);
+    productTotal.innerText = productNumber * price;
 }
 
-// Plus button handler
-document.querySelector('#case-plus').addEventListener('click', () => {
-    updateCaseNumber(true);
+// increase decrease event handler
+document.querySelector('#phone-plus').addEventListener('click', () => {
+    updateProductNumber('phone', true, 1219);
 });
 
-// Minus button handler
+document.querySelector('#phone-minus').addEventListener('click', () => {
+    updateProductNumber('phone', false, 1219);
+});
 
+// case increase decrease event handler
+document.querySelector('#case-plus').addEventListener('click', () => {
+    updateProductNumber('case', true, 59);
+});
 document.querySelector('#case-minus').addEventListener('click', () => {
-    updateCaseNumber(false);
+    updateProductNumber('case', false, 59);
 });
