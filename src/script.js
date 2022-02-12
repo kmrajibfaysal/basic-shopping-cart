88a9f57b43b23dd2b2c13d7935745f9b2822c3ce
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-13 00:46:25 +0600

    adding + button handler for count

--- src/script.js
+++ src/script.js
@@ -1,12 +1,34 @@
-// const fib = (n) => {
-//     if (n === 1) {
-//         return [0, 1];
-//     }
-//     const arr = fib(n - 1);
-//     arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
-//     return arr;
+// window.onload = () => {
+//     main();
 // };
 
-// const a = 5;
+// function main() {
+//     // alert('hi');
+// }
 
-// const b = 4;
+// const casePlus = document.querySelector('#case-plus');
+// const caseMinus = document.querySelector('#case-minus');
+// const phoneQuantity = document.querySelector('#case-count-phone');
+// const phonePrice = document.querySelector('#phone-price');
+
+// casePlus.addEventListener('click', () => {
+//     let phoneCount = parseInt(phoneQuantity.value);
+//     phoneCount += 1;
+//     phoneQuantity.value = phoneCount;
+//     const phoneTotalPrice = parseInt(phonePrice.innerText);
+//     phonePrice.innerText = phoneTotalPrice * phoneQuantity.value;
+// });
+
+// caseMinus.addEventListener('click', () => {
+//     if (phoneQuantity.value > 0) {
+//         let phoneCount = parseInt(phoneQuantity.value);
+//         phoneCount -= 1;
+//         phoneQuantity.value = phoneCount;
+//     }
+// });
+
+document.querySelector('#case-plus').addEventListener('click', () => {
+    const caseInput = document.querySelector('#case-count-phone');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+});
